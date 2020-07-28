37cf028579f591498f6fb1630b5590e83c88f128
Ryuji-commit <56810015+Ryuji-commit@users.noreply.github.com>
2020-07-28 16:00:58 +0900

    implement Quick sort.

--- Sort/Insertion_Sort.py
+++ Sort/Insertion_Sort.py
@@ -3,22 +3,16 @@ import time
 
 
 def insert(a, pos, value):
-    counter = 1
     i = pos - 1
     while i >= 0 and a[i] > value:
         a[i+1] = a[i]
         i -= 1
-        counter += 1
     a[i+1] = value
-    return counter
 
 
 def sort(a):
-    process_counter = 0
     for pos in range(1, len(a)):
-        process_counter += insert(a, pos, a[pos])
-
-    return process_counter
+        insert(a, pos, a[pos])
 
 
 if __name__ == "__main__":
@@ -27,9 +21,8 @@ if __name__ == "__main__":
     print('List length : {}'.format(ListLength))
     print('before Insertion sort : {}'.format(A))
     start_time = time.time()
-    process_number = sort(A)
+    sort(A)
     elapsed_time = time.time() - start_time
     print('sorted : {}'.format(A))
-    print('process count : {}'.format(process_number))
     print('elapsed time : {}sec'.format(elapsed_time))
 
