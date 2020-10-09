4e4a6b3b39115fad719a77b359cba9bd1e647c74
Ryuji-commit <56810015+Ryuji-commit@users.noreply.github.com>
2020-10-09 14:56:23 +0900

    change quick sort

--- sort_test.py
+++ sort_test.py
@@ -3,7 +3,7 @@ from Sort import quick_sort
 
 def main():
     unsorted_list = [6, 8, 1]
-    quick_sort.quick_sort(unsorted_list, 0, 2)
+    quick_sort.quick_sort(unsorted_list)
     print(unsorted_list)
 
 
